e2904c595e31e32588754cd0ed4e3f09ef47f084
KevinJN03 <114575604+KevinJN03@users.noreply.github.com>
2022-11-25 10:15:28 +0000

    updated

--- react-app/src/Todos.js
+++ react-app/src/Todos.js
@@ -1,5 +1,6 @@
 
- const Removetodo = (id,) => {
+ const Removetodo = (id) => {
+    console.log(id)
     fetch(`http://localhost:5001/post/delete/${id}`, {
         method: 'DELETE'
     })
@@ -15,7 +16,7 @@ return(
 <section className= "text-container">
     <h2>{Text}</h2>
     <h3>{id}</h3>
-    {<button onClick={Removetodo}>delete</button>}
+    {<button>delete</button>}
     <button>update</button>
 </section>
 
