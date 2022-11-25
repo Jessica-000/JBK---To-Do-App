eee23d39512b2ab479d73f21f2143e0386266b58
KevinJN03 <114575604+KevinJN03@users.noreply.github.com>
2022-11-25 09:25:56 +0000

    added delet and update button

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from "react"
+import Todos from './Todos';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [error, setError]= useState(null)
+const [todo, setTodo] = useState([]);
+  const [newtodo, setNewtodo] = useState({
+    Text: ""
+  });
+
+useEffect(() => {
+fetch('http://localhost:5001/post/')
+.then(res => res.json())
+.then(res => {
+  setTodo(res.todo)
+}, error => {
+  setError(error)
+})
+}, [])
+
+useEffect(() => {
+  if(newtodo.Text !== ""){
+fetch("http://localhost:5001/post/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newtodo)
+    })
+    .then(res => res.json())
+    .then(res => setTodo(res.todo), error => setError(error))
+  }
+}, [newtodo])
+    
+
+
+
+function submitclick(){
+  setNewtodo({
+    Text: document.getElementById("todoinput").value
+  })
+}
+
+if(error){
+  return <div className = "todo">Error {error.message}</div>
+}else {
+  return(
+    <div className= "App">
+
+      <header></header>
+
+      <section className='todoadder'>
+    <input type= "text" placeholder="Enter ToDo" id="todoinput"/>
+    <button onClick={submitclick}>Add ToDo</button>
+      </section>
+    
+<section className = "todolist-container">
+      {todo.map( (post,key) => <Todos Text={post.Text} id ={post.id} key={key} />)}
+      </section>
+
+    <footer></footer>
     </div>
-  );
+    
+  )
+}
 }
 
 export default App;
